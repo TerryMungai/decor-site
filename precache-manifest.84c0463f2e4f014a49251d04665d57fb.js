804838ccf9cf8a0f95eb42c0c647e4a4fb7287ff
TerryMungai <terymungai@gmail.com>
2021-01-11 17:27:17 +0300

    Updates

---
+++ precache-manifest.84c0463f2e4f014a49251d04665d57fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7050d69ead7cad1e3b84b9a3a935adfb",
+    "url": "/greensfarm-site/index.html"
+  },
+  {
+    "revision": "d52ec0e8ae1b107b06f7",
+    "url": "/greensfarm-site/static/css/main.528f0d19.chunk.css"
+  },
+  {
+    "revision": "e331dddbbaaa9defa76f",
+    "url": "/greensfarm-site/static/js/2.83ff8c38.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/greensfarm-site/static/js/2.83ff8c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52ec0e8ae1b107b06f7",
+    "url": "/greensfarm-site/static/js/main.acae3d5a.chunk.js"
+  },
+  {
+    "revision": "aefb69a80e6deffc7aae",
+    "url": "/greensfarm-site/static/js/runtime-main.543a95bb.js"
+  }
+]);
